d8e986a4a8093affe07c4b5ea5552482bda0589c
stringthread <string.thread@gmail.com>
2020-08-12 19:15:13 +0900

    Save mixed mp3.

--- index.js
+++ index.js
@@ -4,12 +4,49 @@ const {EventEmitter} = require('events');
 const {Readable}=require('stream')
 const {env}=require('process');
 const encode_voice=require('./encode_voice/encode_voice.js')
+const Mixer=require('audio-mixer');
 require('dotenv').config();
 
 const config = require('./auth.json');
 
 class Silence extends Readable{
   _read(){this.push(Buffer.from([0xF8,0xFF,0xFE]))}
+}
+
+class MixSave{
+  constructor(rs_list,filename){
+    this.filename=filename;
+    this.mixer = new Mixer.Mixer({
+      channels: 2,
+      bitDepth: 16,
+      sampleRate: 48000,
+      clearInterval: 250
+    });
+    for (var i = 0; i < rs_list.length; i++) {
+      this.add_rs(rs_list[i]);
+    }
+    this.ws=fs.createWriteStream(this.filename);
+    this.mixer.pipe(this.ws);
+  }
+  add_rs(rs){
+    var new_in=this.mixer.input({})
+    rs.on('end',(()=>{
+      this.mixer.removeInput(new_in);
+      if(this.mixer.inputs.length==0) this.close();
+    }).bind(this))
+    .on('close',(()=>{
+      this.mixer.removeInput(new_in);
+      if(this.mixer.inputs.length==0) this.close();
+    }).bind(this))
+    .pipe(new_in);
+  }
+  close(){
+    this.mixer.unpipe();
+    this.mixer.close();
+    this.mixer.destroy();
+    this.ws.destroy();
+    encode_voice(this.filename);
+  }
 }
 
 class FileSave extends EventEmitter{
@@ -34,8 +71,9 @@ class Bot{
     fs.mkdir(this.dirpath,()=>{});
   }
   generateOutName(channel, member) {
-    if(!this.dirpath)generateDirPath(channel);
-    return `${this.dirpath}/${member.id}-${Date.now()}.pcm`;
+    //if(!this.dirpath)generateDirPath(channel);
+    //return `${this.dirpath}/${member.id}-${Date.now()}.pcm`;
+    return `./recordings/${channel.name}-${Date.now()}.pcm`;
   }
 
   constructor(id){
@@ -45,6 +83,7 @@ class Bot{
     this.rec_users=new Set();
     this.filesaves=new Set();
     this.dirpath=''
+    this.mixsave=null;
 
     this.client.on('message', (msg => {
       if (msg.content.startsWith(config.prefix+'join')) {
@@ -57,7 +96,7 @@ class Bot{
         if (!voiceChannel || voiceChannel.type !== 'voice') {
           return msg.reply(`I couldn't find the channel ${channelName}. Can you spell?`);
         }
-        this.generateDirPath(voiceChannel);
+        //this.generateDirPath(voiceChannel);
         voiceChannel.join()
           .then(conn => {
             msg.reply('ready!');
@@ -67,14 +106,18 @@ class Bot{
               if (speaking && !this.rec_users.has(user.id)) {
                 console.log(`Speaking: ${user}`)
                 msg.channel.send(`I'm listening to ${user}`);
+                if(!this.mixsave)this.mixsave=new MixSave(
+                  [conn.receiver.createStream(user,{mode:'pcm',end:'manual'})],
+                  this.generateOutName(voiceChannel, user));
+                else this.mixsave.add_rs(conn.receiver.createStream(user,{mode:'pcm',end:'manual'}));
                 this.rec_users.add(user.id);
-                var filesave=new FileSave(conn.receiver.createStream(user,{mode:'pcm',end:'manual'}),this.generateOutName(voiceChannel, user));
+                /*var filesave=new FileSave(conn.receiver.createStream(user,{mode:'pcm',end:'manual'}),this.generateOutName(voiceChannel, user));
                 filesave.on('end', (() => {
                   console.log(`End Speaking: ${user}`);
                   this.rec_users.delete(user.id);
                   this.filesaves.delete(filesave);
                 }).bind(this));
-                this.filesaves.add(filesave);
+                this.filesaves.add(filesave);*/
               }
             });
           })
@@ -87,6 +130,7 @@ class Bot{
         this.rec_users.clear()
         for(var item of this.filesaves){item.encode();}
         this.filesaves.clear();
+        this.mixsave.close();
         this.dirpath='';
       }
     }).bind(this));
